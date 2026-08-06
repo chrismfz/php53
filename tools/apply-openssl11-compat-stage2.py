13ec93b522d3848fe43fe1804af4b89f73d7cb3e
Chris <chrismfz@gmail.com>
2026-08-07 02:02:36 +0300

    Scope OpenSSL context rewrites by function

--- tools/apply-openssl11-compat-stage2.py
+++ tools/apply-openssl11-compat-stage2.py
@@ -30,6 +30,22 @@ def exact(text, old, new, count, label):
     return text.replace(old, new)
 
 
+def transform_php_function(text, function_name, transformer):
+    marker = "PHP_FUNCTION(%s)" % function_name
+    start = text.find(marker)
+    if start < 0:
+        die("function not found: %s" % function_name)
+    end = text.find("\n/* }}} */", start)
+    if end < 0:
+        die("function end not found: %s" % function_name)
+    end += len("\n/* }}} */")
+    block = text[start:end]
+    changed = transformer(block)
+    if changed == block:
+        die("no changes applied in %s" % function_name)
+    return text[:start] + changed + text[end:]
+
+
 src = Path(sys.argv[1] if len(sys.argv) > 1 else os.environ.get(
     "SRC_DIR", "/usr/local/src/ngm-php-build/php-5.3.29"
 ))
@@ -63,17 +79,16 @@ text = once(
 )
 text = text.replace("(EVP_MD *) EVP_dss1()", "(EVP_MD *) EVP_sha1()")
 
-text = exact(text, "\tEVP_MD_CTX md_ctx;", "\tEVP_MD_CTX *md_ctx = NULL;", 2, "EVP_MD_CTX declaration")
-text = once(text, "\tEVP_MD_CTX     md_ctx;", "\tEVP_MD_CTX *md_ctx = NULL;", "EVP verify context declaration")
-text = exact(text, "&md_ctx", "md_ctx", 11, "EVP_MD_CTX address")
-text = exact(text, "EVP_MD_CTX_cleanup(md_ctx);", "EVP_MD_CTX_destroy(md_ctx);", 2, "EVP_MD_CTX cleanup")
 
-text = once(
-    text,
-    '''\tsigbuf = emalloc(siglen + 1);
+def patch_sign(block):
+    block = exact(block, "\tEVP_MD_CTX md_ctx;", "\tEVP_MD_CTX *md_ctx = NULL;", 1, "sign EVP_MD_CTX declaration")
+    block = exact(block, "&md_ctx", "md_ctx", 4, "sign EVP_MD_CTX address")
+    block = once(
+        block,
+        '''\tsigbuf = emalloc(siglen + 1);
 
 \tEVP_SignInit(md_ctx, mdtype);''',
-    '''\tsigbuf = emalloc(siglen + 1);
+        '''\tsigbuf = emalloc(siglen + 1);
 \tmd_ctx = EVP_MD_CTX_create();
 \tif (md_ctx == NULL) {
 \t\tefree(sigbuf);
@@ -84,12 +99,18 @@ text = once(
 \t}
 
 \tEVP_SignInit(md_ctx, mdtype);''',
-    "sign context allocation",
-)
-text = once(
-    text,
-    '''\tEVP_VerifyInit   (md_ctx, mdtype);''',
-    '''\tmd_ctx = EVP_MD_CTX_create();
+        "sign context allocation",
+    )
+    return once(block, "EVP_MD_CTX_cleanup(md_ctx);", "EVP_MD_CTX_destroy(md_ctx);", "sign context free")
+
+
+def patch_verify(block):
+    block = once(block, "\tEVP_MD_CTX     md_ctx;", "\tEVP_MD_CTX *md_ctx = NULL;", "verify EVP_MD_CTX declaration")
+    block = exact(block, "&md_ctx", "md_ctx", 4, "verify EVP_MD_CTX address")
+    block = once(
+        block,
+        '''\tEVP_VerifyInit   (md_ctx, mdtype);''',
+        '''\tmd_ctx = EVP_MD_CTX_create();
 \tif (md_ctx == NULL) {
 \t\tif (keyresource == -1) {
 \t\t\tEVP_PKEY_free(pkey);
@@ -97,14 +118,20 @@ text = once(
 \t\tRETURN_FALSE;
 \t}
 \tEVP_VerifyInit   (md_ctx, mdtype);''',
-    "verify context allocation",
-)
-text = once(
-    text,
-    '''\tsigbuf = emalloc(siglen + 1);
+        "verify context allocation",
+    )
+    return once(block, "EVP_MD_CTX_cleanup(md_ctx);", "EVP_MD_CTX_destroy(md_ctx);", "verify context free")
+
+
+def patch_digest(block):
+    block = exact(block, "\tEVP_MD_CTX md_ctx;", "\tEVP_MD_CTX *md_ctx = NULL;", 1, "digest EVP_MD_CTX declaration")
+    block = exact(block, "&md_ctx", "md_ctx", 3, "digest EVP_MD_CTX address")
+    block = once(
+        block,
+        '''\tsigbuf = emalloc(siglen + 1);
 
 \tEVP_DigestInit(md_ctx, mdtype);''',
-    '''\tsigbuf = emalloc(siglen + 1);
+        '''\tsigbuf = emalloc(siglen + 1);
 \tmd_ctx = EVP_MD_CTX_create();
 \tif (md_ctx == NULL) {
 \t\tefree(sigbuf);
@@ -112,119 +139,134 @@ text = once(
 \t}
 
 \tEVP_DigestInit(md_ctx, mdtype);''',
-    "digest context allocation",
-)
-text = once(
-    text,
-    '''\t} else {
+        "digest context allocation",
+    )
+    return once(
+        block,
+        '''\t} else {
 \t\tefree(sigbuf);
 \t\tRETVAL_FALSE;
 \t}
-}
-/* }}} */
-
-static zend_bool php_openssl_validate_iv''',
-    '''\t} else {
+}''',
+        '''\t} else {
 \t\tefree(sigbuf);
 \t\tRETVAL_FALSE;
 \t}
 \tEVP_MD_CTX_destroy(md_ctx);
-}
-/* }}} */
+}''',
+        "digest context free",
+    )
 
-static zend_bool php_openssl_validate_iv''',
-    "digest context free",
-)
 
-text = exact(text, "\tEVP_CIPHER_CTX ctx;", "\tEVP_CIPHER_CTX *ctx = NULL;", 2, "EVP_CIPHER_CTX ctx declaration")
-text = exact(text, "\tEVP_CIPHER_CTX cipher_ctx;", "\tEVP_CIPHER_CTX *cipher_ctx = NULL;", 2, "EVP_CIPHER_CTX cipher declaration")
-text = exact(text, "&ctx", "ctx", 9, "EVP_CIPHER_CTX ctx address")
-text = exact(text, "&cipher_ctx", "cipher_ctx", 12, "EVP_CIPHER_CTX cipher address")
-
-text = once(
-    text,
-    '''\tif (!EVP_EncryptInit(ctx,cipher,NULL,NULL)) {''',
-    '''\tctx = EVP_CIPHER_CTX_new();
+def patch_seal(block):
+    block = exact(block, "\tEVP_CIPHER_CTX ctx;", "\tEVP_CIPHER_CTX *ctx = NULL;", 1, "seal EVP_CIPHER_CTX declaration")
+    block = exact(block, "&ctx", "ctx", 6, "seal EVP_CIPHER_CTX address")
+    block = once(
+        block,
+        '''\tif (!EVP_EncryptInit(ctx,cipher,NULL,NULL)) {''',
+        '''\tctx = EVP_CIPHER_CTX_new();
 \tif (ctx == NULL || !EVP_EncryptInit(ctx,cipher,NULL,NULL)) {''',
-    "seal context allocation",
-)
-text = once(
-    text,
-    '''clean_exit:
+        "seal context allocation",
+    )
+    return once(
+        block,
+        '''clean_exit:
 \tfor (i=0; i<nkeys; i++) {''',
-    '''clean_exit:
+        '''clean_exit:
 \tEVP_CIPHER_CTX_free(ctx);
 \tfor (i=0; i<nkeys; i++) {''',
-    "seal context free",
-)
-text = once(
-    text,
-    '''\tbuf = emalloc(data_len + 1);
+        "seal context free",
+    )
+
+
+def patch_open(block):
+    block = exact(block, "\tEVP_CIPHER_CTX ctx;", "\tEVP_CIPHER_CTX *ctx = NULL;", 1, "open EVP_CIPHER_CTX declaration")
+    block = exact(block, "&ctx", "ctx", 3, "open EVP_CIPHER_CTX address")
+    block = once(
+        block,
+        '''\tbuf = emalloc(data_len + 1);
 
 \tif (EVP_OpenInit(ctx, cipher,''',
-    '''\tbuf = emalloc(data_len + 1);
+        '''\tbuf = emalloc(data_len + 1);
 \tctx = EVP_CIPHER_CTX_new();
 
 \tif (ctx != NULL && EVP_OpenInit(ctx, cipher,''',
-    "open context allocation",
-)
-text = exact(
-    text,
-    '''\t\tefree(buf);
+        "open context allocation",
+    )
+    block = exact(
+        block,
+        '''\t\tefree(buf);
 \t\tif (keyresource == -1) {''',
-    '''\t\tefree(buf);
+        '''\t\tefree(buf);
 \t\tEVP_CIPHER_CTX_free(ctx);
 \t\tif (keyresource == -1) {''',
-    2,
-    "open failure context free",
-)
-text = once(
-    text,
-    '''\tif (keyresource == -1) {
+        2,
+        "open failure context free",
+    )
+    return once(
+        block,
+        '''\tif (keyresource == -1) {
 \t\tEVP_PKEY_free(pkey);
 \t}
 \tzval_dtor(opendata);''',
-    '''\tEVP_CIPHER_CTX_free(ctx);
+        '''\tEVP_CIPHER_CTX_free(ctx);
 \tif (keyresource == -1) {
 \t\tEVP_PKEY_free(pkey);
 \t}
 \tzval_dtor(opendata);''',
-    "open success context free",
-)
+        "open success context free",
+    )
 
-text = exact(
-    text,
-    '''\tkeylen = EVP_CIPHER_key_length(cipher_type);''',
-    '''\tcipher_ctx = EVP_CIPHER_CTX_new();
+
+def patch_encrypt(block):
+    block = exact(block, "\tEVP_CIPHER_CTX cipher_ctx;", "\tEVP_CIPHER_CTX *cipher_ctx = NULL;", 1, "encrypt EVP_CIPHER_CTX declaration")
+    block = exact(block, "&cipher_ctx", "cipher_ctx", 6, "encrypt EVP_CIPHER_CTX address")
+    block = once(
+        block,
+        '''\tkeylen = EVP_CIPHER_key_length(cipher_type);''',
+        '''\tcipher_ctx = EVP_CIPHER_CTX_new();
 \tif (cipher_ctx == NULL) {
 \t\tphp_error_docref(NULL TSRMLS_CC, E_WARNING, "Failed to create cipher context");
 \t\tRETURN_FALSE;
 \t}
 
 \tkeylen = EVP_CIPHER_key_length(cipher_type);''',
-    2,
-    "encrypt/decrypt context allocation",
-)
-text = exact(
-    text,
-    "EVP_CIPHER_CTX_cleanup(cipher_ctx);",
-    "EVP_CIPHER_CTX_free(cipher_ctx);",
-    2,
-    "encrypt/decrypt context free",
-)
+        "encrypt context allocation",
+    )
+    return once(block, "EVP_CIPHER_CTX_cleanup(cipher_ctx);", "EVP_CIPHER_CTX_free(cipher_ctx);", "encrypt context free")
 
-text = once(
-    text,
-    '''\tEVP_PKEY *pkey;
+
+def patch_decrypt(block):
+    block = exact(block, "\tEVP_CIPHER_CTX cipher_ctx;", "\tEVP_CIPHER_CTX *cipher_ctx = NULL;", 1, "decrypt EVP_CIPHER_CTX declaration")
+    block = exact(block, "&cipher_ctx", "cipher_ctx", 6, "decrypt EVP_CIPHER_CTX address")
+    block = once(
+        block,
+        '''\tkeylen = EVP_CIPHER_key_length(cipher_type);''',
+        '''\tcipher_ctx = EVP_CIPHER_CTX_new();
+\tif (cipher_ctx == NULL) {
+\t\tphp_error_docref(NULL TSRMLS_CC, E_WARNING, "Failed to create cipher context");
+\t\tRETURN_FALSE;
+\t}
+
+\tkeylen = EVP_CIPHER_key_length(cipher_type);''',
+        "decrypt context allocation",
+    )
+    return once(block, "EVP_CIPHER_CTX_cleanup(cipher_ctx);", "EVP_CIPHER_CTX_free(cipher_ctx);", "decrypt context free")
+
+
+def patch_dh_compute_key(block):
+    block = once(
+        block,
+        '''\tEVP_PKEY *pkey;
 \tBIGNUM *pub;''',
-    '''\tEVP_PKEY *pkey;
+        '''\tEVP_PKEY *pkey;
 \tDH *dh;
 \tBIGNUM *pub;''',
-    "DH declaration",
-)
-text = once(
-    text,
-    '''\tif (!pkey || EVP_PKEY_type(pkey->type) != EVP_PKEY_DH || !pkey->pkey.dh) {
+        "DH declaration",
+    )
+    return once(
+        block,
+        '''\tif (!pkey || EVP_PKEY_type(pkey->type) != EVP_PKEY_DH || !pkey->pkey.dh) {
 \t\tRETURN_FALSE;
 \t}
 
@@ -232,7 +274,7 @@ text = once(
 
 \tdata = emalloc(DH_size(pkey->pkey.dh) + 1);
 \tlen = DH_compute_key((unsigned char*)data, pub, pkey->pkey.dh);''',
-    '''\tif (!pkey || EVP_PKEY_base_id(pkey) != EVP_PKEY_DH) {
+        '''\tif (!pkey || EVP_PKEY_base_id(pkey) != EVP_PKEY_DH) {
 \t\tRETURN_FALSE;
 \t}
 \tdh = (DH *)EVP_PKEY_get0_DH(pkey);
@@ -247,8 +289,18 @@ text = once(
 
 \tdata = emalloc(DH_size(dh) + 1);
 \tlen = DH_compute_key((unsigned char*)data, pub, dh);''',
-    "DH accessor",
-)
+        "DH accessor",
+    )
+
+
+text = transform_php_function(text, "openssl_sign", patch_sign)
+text = transform_php_function(text, "openssl_verify", patch_verify)
+text = transform_php_function(text, "openssl_seal", patch_seal)
+text = transform_php_function(text, "openssl_open", patch_open)
+text = transform_php_function(text, "openssl_digest", patch_digest)
+text = transform_php_function(text, "openssl_encrypt", patch_encrypt)
+text = transform_php_function(text, "openssl_decrypt", patch_decrypt)
+text = transform_php_function(text, "openssl_dh_compute_key", patch_dh_compute_key)
 
 if text == original:
     die("no openssl.c changes applied")
