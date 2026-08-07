90ffabe748fc7f70744b8231a059143569b812b3
ngm-php-maintenance <ngm-php-maintenance@users.noreply.github.com>
2026-08-07 10:17:48 +0000

    Drop SNMP from shared hosting runtime

--- build-php53.sh
+++ build-php53.sh
@@ -204,8 +204,7 @@ install_deps() {
         libxml2-devel gnutls-devel nettle-devel libjpeg-turbo-devel libpng-devel \
         freetype-devel bzip2-devel readline-devel libxslt-devel \
         gmp-devel sqlite-devel zlib-devel gettext-devel libxcrypt-devel \
-        libpq-devel openldap-devel cyrus-sasl-devel libtidy-devel aspell-devel \
-
+        libpq-devel openldap-devel cyrus-sasl-devel libtidy-devel aspell-devel
       ;;
   esac
 }
